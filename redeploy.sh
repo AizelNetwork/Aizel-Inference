87def15c7fd0f7f97870ecfa2ecfbd30236d9505
WillJiang1 <jianliiin96@gmail.com>
2024-06-12 15:50:15 +0000

    feat: download before registration

--- redeploy.sh
+++ redeploy.sh
@@ -14,10 +14,8 @@ gcloud compute instances create inference-demo \
     --image-project=confidential-space-images \
     --image-family=confidential-space-debug \
     --service-account=991449629434-compute@developer.gserviceaccount.com \
-    --metadata="^~^tee-image-reference=asia-docker.pkg.dev/bionic-mercury-421809/aizel/aizel_inference:0.1.0~tee-env-CHAIN_ID=11155111~tee-env-ENDPOINT=https://sepolia.infura.io/v3/4edd867b9c3c4999bac4b3aaea9842b7~tee-env-PRIVATE_KEY=647fcb49c378e22dc51a5fd43b3b76b28f00f605191ed7d419e1080854711cae~tee-env-CONTRACT_ADDRESS=0x94539d83c832Eb6011A32C2E7aC2BD71fa7FC894~tee-container-log-redirect=true" \
-    --machine-type=n2d-standard-4 \
+    --metadata="^~^tee-image-reference=asia-docker.pkg.dev/bionic-mercury-421809/aizel/aizel_inference:0.1.0~tee-env-CHAIN_ID=4369~tee-env-ENDPOINT=http://34.124.144.235:9944~tee-env-PRIVATE_KEY=647fcb49c378e22dc51a5fd43b3b76b28f00f605191ed7d419e1080854711cae~tee-env-CONTRACT_ADDRESS=0x5F9BAe82718B469721C6CD55D6Ab356dc5D60c5B~tee-container-log-redirect=true" \
+    --machine-type=n2d-standard-16 \
     --min-cpu-platform="AMD Milan" \
     --boot-disk-size=50 \
     --tags=will-dev
-
-# ENDPOINT=https://sepolia.infura.io/v3/4edd867b9c3c4999bac4b3aaea9842b7 PRIVATE_KEY=647fcb49c378e22dc51a5fd43b3b76b28f00f605191ed7d419e1080854711cae CONTRACT_ADDRESS=0x94539d83c832Eb6011A32C2E7aC2BD71fa7FC894 CHAIN_ID=11155111 cargo test --package aizel_inference --lib -- node::server::tests::test_contract --exact --show-output
